917bf5c242f3c86234a9796e20da5237a7e1d656
julian <juka1031@gmail.com>
2021-07-27 17:28:42 -0400

    e

---
+++ music_app/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    def new
+        @user= User.new
+        render :new
+    end
+
+    def show
+        render :show
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            redirect_to user_url
+        else
+            render :new
+        end
+    end
+
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+
+end
